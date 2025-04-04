2301b05f5dc32af0fae8353094659aee117b5247
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-04-04 14:29:46 +0500

    texnikum

--- news/models.py
+++ news/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class News(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     title = models.CharField(max_length=250)
-    body = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='news/', null=True, blank=True)
+    body = models.TextField()
+    image = models.ImageField(upload_to='news/')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     uploaded_at = models.DateTimeField(default=timezone.now)
 
--- worker/models.py
+++ worker/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Worker(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     name = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='worker/', null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    photo = models.ImageField(upload_to='worker/' )
+    description = models.TextField()
 
     def __str__(self):
         return self.name
