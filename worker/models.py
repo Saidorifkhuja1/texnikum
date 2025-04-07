4dd470c76edf1c4039e770070c962894c597e5ac
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-04-07 16:43:09 +0500

    texnikum

--- worker/models.py
+++ worker/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Worker(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    name = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='worker/' )
-    description = models.TextField()
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='worker/' )
+    body = models.TextField()
 
     def __str__(self):
         return self.name
